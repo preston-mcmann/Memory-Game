59c43a20f93a96a66f1f8d17d411a0f0d30bc261
ahuyn9 <ahuyn9@ocdsb.ca>
2022-05-12 10:57:19 -0400

    class sets created

---
+++ src/memory/game/Sets.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package memory.game;
+
+/**
+ *
+ * @author ahuyn9
+ */
+public class Sets {
+   String title = "";
+   int cardNum = 0;
+}
