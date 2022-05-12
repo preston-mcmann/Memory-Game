ac64ef6068519df7e61cacc978822ffa8056c3a7
ahuyn9 <ahuyn9@ocdsb.ca>
2022-05-12 10:57:41 -0400

    class cards created

---
+++ src/memory/game/Cards.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package memory.game;
+
+/**
+ *
+ * @author ahuyn9
+ */
+public class Cards {
+    String term = "";
+    String defintiion = "";
+    
+}
