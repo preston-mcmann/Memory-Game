23f49460b01c7dbc7ed014c1792f1ab71ee80d04
ahuyn9 <ahuyn9@ocdsb.ca>
2022-05-16 11:32:01 -0400

    sets updated

--- src/memory/game/Sets.java
+++ src/memory/game/Sets.java
@@ -4,15 +4,46 @@
  */
 package memory.game;
 
+import java.util.List;
+
 /**
  *
  * @author mcman
  */
 public class Sets {
+
     String name;
-    public Sets(String name){
-        this.name=name;
+    private static List<Cards> cards;
+
+    public Sets(String name, List<Cards> cards) {
+        this.name = name;
+        this.cards = cards;
     }
+
+    public static List<Cards> getCards() {
+        return cards;
+    }
+
+    public void addCards(List<Cards> cards) {
+        cards.addAll(cards);
+    }
+
+    public Cards makeCards(String term, String def) {
+        Cards Card = new Cards(term, def);
+        return Card;
+    }
+
+    public void print() {
+        for (Cards card : cards) {
+            System.out.println(card.getTerm());
+            System.out.println(card.getDefinition());
+        }
+    }
+
+    public static void setCards(List<Cards> cards) {
+        Sets.cards = cards;
+    }
+
     public String getName() {
         return name;
     }
@@ -20,5 +51,4 @@ public class Sets {
     public void setName(String name) {
         this.name = name;
     }
-    
 }
