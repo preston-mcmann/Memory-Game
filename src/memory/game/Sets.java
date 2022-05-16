ee3de880fa749231cd3706548d3767263387852e
preston-mcmann <102309425+preston-mcmann@users.noreply.github.com>
2022-05-16 10:39:14 -0400

    Added Edit Button

--- src/memory/game/Sets.java
+++ src/memory/game/Sets.java
@@ -4,14 +4,38 @@
  */
 package memory.game;
 
+import java.util.List;
+
 /**
  *
  * @author mcman
  */
 public class Sets {
     String name;
-    public Sets(String name){
+    private static List<Cards> cards;
+    public Sets(String name, List<Cards> cards){
         this.name=name;
+        this.cards=cards;
+    }
+    
+    public static List<Cards> getCards() {
+        return cards;
+    }
+    public void addCards(List<Cards> cards){
+        cards.addAll(cards);
+    }
+    public Cards makeCards(String term, String def){
+        Cards Card = new Cards(term,def);
+        return Card;
+    }
+    public void print(){
+        for (Cards card:cards){
+            System.out.println(card.getTerm());
+            System.out.println(card.getDefinition());
+        }
+    }
+    public static void setCards(List<Cards> cards) {
+        Sets.cards = cards;
     }
     public String getName() {
         return name;
